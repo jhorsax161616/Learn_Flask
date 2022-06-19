93458ee589704be7c8b3c5757939c1fbabedb61d
Jhorsax16 <jhorsax161616@gmail.com>
2022-06-19 13:39:12 -0500

    Web dynamism with for and if

--- app/app.py
+++ app/app.py
@@ -5,34 +5,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return render_template('index.html', title='My Firts Web App')
+    # return render_template('index.html', title='My Firts Web App')
     data = {
         'title': 'My First Web App',
-        'user_name' : 'Jhordan',
-        'user_age' : 25
+        'user_name': 'Jhordan',
+        'user_age': 25
     }
     return render_template('index.html', data=data)
+
 
 @app.route('/contact')
 def contact():
     data = {
         'title': 'Contact',
-        'user_name' : 'Jhordan',
-        'user_age' : 25
+        'user_name': 'Jhordan',
+        'user_age': 25
     }
     return render_template('contact.html', data=data)
+
 
 @app.route('/greeting/<user_name>')
 def greeting(user_name):
     return f'!Hi, {user_name}!'
 
+
 @app.route('/sum/<int:value1>/<int:value2>')
 def sum(value1, value2):
     return "The sum is: " + str(value1 + value2)
 
+
 @app.route('/profile/<user_name>/<int:age>')
-def profile(user_name, age :int):
+def profile(user_name, age: int):
     return 'Your name is {0} and your age is {1} years old'.format(user_name, age)
+
+
+@app.route('/languages')
+def languages():
+    data = {
+        'available' : False,
+        'languages': ['PHP', 'Python', 'Kotlin', 'Java', 'C#', 'JavaScript']
+    }
+    return render_template('languages.html', data=data)
+
 
 def helloWorld():
     return {
